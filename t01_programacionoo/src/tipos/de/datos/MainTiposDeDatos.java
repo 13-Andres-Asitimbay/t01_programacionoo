8b8614aabd4e399891f600d8cf6f738107bcb35e
AndrosBY <AndrosBY@AndrosPC-13>
2021-07-21 18:50:21 -0500

    ahora

---
+++ t01_programacionoo/src/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,21 @@
+package tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//No se deben usar caracteres especiales como ñ, tildes, etc. Tampoco usar mayusculas para todo.
+		short cantidadEstudiantes = 1000;
+		byte entradaCircuito = 0;
+		int cantidadCiudadanos = 100000;
+		long cantidadMundo = 1000000000;
+		float sueldo = 1200.56F;
+		float sueldo2 = 1200.56f;
+		double saldoBanco = 100000000.67;
+		char humanoCaido = 'C';
+		char opcionMenu = 'M';
+		boolean casado = true;
+		boolean soltero = false;
+	}
+
+}
